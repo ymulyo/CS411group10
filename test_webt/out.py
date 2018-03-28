d9b670c5667c77c431f7124058f03ea7d2879c0f
Yusuf Mulyo <yusufmulyo@dhcp-wifi-8021x-155-41-41-93.bu.edu>
2018-03-27 21:51:55 -0400

    Assignment 3

---
+++ test_webt/out.py
@@ -0,0 +1,45 @@
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+import http.client
+import json
+
+@app.route('/success/<name>')
+def success(name):
+    return 'Jobs: %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+    host = 'us.jooble.org'
+    key = 'e638b80e-d61b-45f8-9c19-97d37ac76e8e'
+
+    connection = http.client.HTTPConnection(host)
+    #request headers
+    headers = {"Content-type": "application/x-www-form-urlencoded"}
+    if request.method == 'POST':
+        user = request.form['nm']
+        loc = request.form['nn']
+    else:
+        user = request.args.get('nm')
+        loc = request.form['nn']
+    body = '{ "keywords": "' + user + '", "location": "' + loc + '"}'
+    connection.request('POST','/api/' + key, body, headers)
+    response = connection.getresponse()
+    j = response.read()
+    j = json.loads(j)
+    jobs = j['jobs']
+    max = 5
+    if len(jobs) < 5:
+        max = len(jobs)
+    for i in range(0,max):
+        fstJob = jobs[i]
+        if(i == 0):
+            user = fstJob["company"]
+        elif(i == max-1):
+            user += ', and ' + fstJob["company"] + '.'
+        else:
+            user += ', ' + fstJob["company"]
+    return redirect(url_for('success',name = user))
+
+if __name__ == '__main__':
+    app.run(debug = True)
