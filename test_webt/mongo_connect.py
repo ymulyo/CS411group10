7efb45cefe907d535138a18a6a73e520967814e4
kamccue <34356248+kamccue@users.noreply.github.com>
2018-04-19 22:59:40 -0400

    Add files via upload

---
+++ test_webt/mongo_connect.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask.ext.pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'connect_to_mongo'
+#app.config['MONGO_URI'] = 
+
+mongo = PyMongo(app)
+
+@app.route('/add')
+def add():
+	user = mongo.db.users
+	user.insert({'name' : 'KayceMcCue'})
+	return 'Added User!'
+
+@app.route('/find')
+def find():
+	user = mongo.db.users
+	found = user.find_one({'name' : 'KayceMcCue'})
+	return 'You found ' + found['name']
+
+if __name__ == '__main__':
+	app.run(debug=True)
