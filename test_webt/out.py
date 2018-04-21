fe440b36855038eac0143b3ea614b6de0d50f920
Yusuf Mulyo <yusufmulyo@Yusufs-MacBook-Pro.local>
2018-04-20 20:55:07 -0400

    First changes for assignment3

--- test_webt/out.py
+++ test_webt/out.py
@@ -1,7 +1,13 @@
 import config
 
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, render_template
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = config.dbName()
+app.config['MONGO_URI'] = config.mongoURI()
+
+mongo = PyMongo(app)
 
 import http.client
 import json
@@ -24,12 +30,25 @@ def login():
     else:
         user = request.args.get('nm')
         loc = request.args.get('nn')
-    body = '{ "keywords": "' + user + '", "location": "' + loc + '"}'
-    connection.request('POST','/api/' + key, body, headers)
-    response = connection.getresponse()
-    j = response.read()
-    j = json.loads(j)
-    jobs = j['jobs']
+    
+    dbUser = mongo.db.users
+    jobs = []
+
+    if(dbUser.count({'keyword' : user, 'location' : loc}) > 0):
+        found = dbUser.find_one({'keyword' : user, 'location' : loc})
+        jobs = found['jobs']
+        print('cache') #Indicates that this has been search before and it is found in the db
+    else:
+        body = '{ "keywords": "' + user + '", "location": "' + loc + '"}'
+        connection.request('POST','/api/' + key, body, headers)
+        response = connection.getresponse()
+        j = response.read()
+        j = json.loads(j)
+        j.update({'keyword' : user, 'location' : loc})
+        jobs = j['jobs']
+        dbUser.insert(j)
+        print('new') #Indicates that this is a new search 
+
     max = 5
     if len(jobs) < 5:
         max = len(jobs)
@@ -42,6 +61,14 @@ def login():
         else:
             user += ', ' + fstJob["company"]
     return redirect(url_for('success',name = user))
+    #return redirect(url_for('emails',name = user))
+    #return render_template('results.html', email_addresses=user)
+
+@app.route('/')
+def hello_world():
+    author = "Me"
+    name = "You"
+    return render_template('index.html', author=author, name=name)
 
 if __name__ == '__main__':
     app.run(debug = True)
