fdb3ea2240101b891017c8b95c667a32f5ff5878
Yusuf Mulyo <yusufmulyo@Yusufs-MacBook-Pro.local>
2018-04-29 01:55:40 -0400

    changes in oauth and search

--- test_webt/out.py
+++ test_webt/out.py
@@ -1,16 +1,44 @@
 import config
 
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, render_template, session, jsonify
 from flask_pymongo import PyMongo
+from flask_oauthlib.client import OAuth
+from datetime import datetime
+
+import bcrypt
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = config.dbName()
 app.config['MONGO_URI'] = config.mongoURI()
 
+oauth = OAuth(app)
+
 mongo = PyMongo(app)
+
+
 
 import http.client
 import json
+
+hashpass = ''
+
+linkedin = oauth.remote_app(
+    'linkedin',
+    consumer_key= config.consumerKey(),
+    consumer_secret= config.consumerSecret(),
+    request_token_params={
+        'scope': 'r_basicprofile',
+        'scope': 'r_emailaddress',
+        'scope': 'rw_company_admin',
+        'scope': 'w_share',
+        'state': 'DCEEFWF45453sdffef424',
+    },
+    base_url='https://api.linkedin.com/v1/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://www.linkedin.com/uas/oauth2/accessToken',
+    authorize_url='https://www.linkedin.com/uas/oauth2/authorization',
+)
 
 @app.route('/success/<name>')
 def success(name):
@@ -33,18 +61,35 @@ def search():
     
     dbUser = mongo.db.users
     jobs = []
-
-    if(dbUser.count({'keyword' : user, 'location' : loc}) > 0):
-        found = dbUser.find_one({'keyword' : user, 'location' : loc})
+    found = dbUser.find_one({'keyword' : user, 'location' : loc})
+    if found:
+        if 'username' in found:
+            if 'username' in session and not(session['username'] in found['username']):
+                found['username'].append(session['username'])
+            elif(not('guest' in found['username']) and not('username' in session)):
+                found['username'].append('guest')
+        else:
+            name = ''
+            if not('username' in session):
+                name = 'guest'
+            else: 
+                name = found['username']
+            found.update({'username' : [name]})
         jobs = found['jobs']
-        print('cache') #Indicates that this has been search before and it is found in the db
+        print('cache') #Indicates that this has been search before and it is found in the db:
+            
     else:
         body = '{ "keywords": "' + user + '", "location": "' + loc + '"}'
         connection.request('POST','/api/' + key, body, headers)
         response = connection.getresponse()
         j = response.read()
         j = json.loads(j)
-        j.update({'keyword' : user, 'location' : loc})
+        name = ''
+        if not('username' in session):
+            name = 'guest'
+        else: 
+            name = found['username']
+        j.update({'keyword' : user, 'location' : loc, 'username' : [name]})
         jobs = j['jobs']
         dbUser.insert(j)
         print('new') #Indicates that this is a new search 
@@ -64,11 +109,87 @@ def search():
     #return redirect(url_for('emails',name = user))
     #return render_template('results.html', email_addresses=user)
 
+@app.route('/loggedin')
+def loggedin():
+	return 'yay'
+
 @app.route('/')
-def hello_world():
-    author = "Me"
-    name = "You"
-    return render_template('index.html', author=author, name=name)
+def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+
+    return render_template('index.html')
+
+@app.route('/logout')
+def logout():
+	session.clear()
+	print(session)
+	return 'You are logged out!'
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
+
+@app.route('/login2')
+def login2():
+    return linkedin.authorize(callback=url_for('authorized', _external=True))
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('login2'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
+
+@app.route('/login/authorized')
+def authorized():
+    resp = linkedin.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['linkedin_token'] = (resp['access_token'], '')
+    me = linkedin.get('people/~')
+    #append data to mongodb
+    return redirect(url_for('index'))
+
+
+@linkedin.tokengetter
+def get_linkedin_oauth_token():
+    return session.get('linkedin_token')
+
+
+def change_linkedin_query(uri, headers, body):
+    auth = headers.pop('Authorization')
+    headers['x-li-format'] = 'json'
+    if auth:
+        auth = auth.replace('Bearer', '').strip()
+        if '?' in uri:
+            uri += '&oauth2_access_token=' + auth
+        else:
+            uri += '?oauth2_access_token=' + auth
+    return uri, headers, body
+
+linkedin.pre_request = change_linkedin_query
 
 if __name__ == '__main__':
+    app.secret_key = 'mysecret'
     app.run(debug = True)
